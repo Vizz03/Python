6d0f645aa1f0017860b8c4236f00ba21b1b8c05d
Tinashe "Viz" Matembo <31240908+Vizz03@users.noreply.github.com>
2021-01-13 10:38:46 +0200

    Add files via upload

---
+++ world-clock-app/worldclock.py
@@ -0,0 +1,17 @@
+from tkinter import *
+from tkinter.ttk import *
+from time import strftime
+
+root=Tk()
+root.title("world Clock")
+
+def time():
+    string=strftime('%H:%M:%S %p')
+    lbl.config(text=string)
+    lbl.after(1000,time)
+
+lbl=Label(root,font=('calibri',40,'bold'),background='orange',foreground='white')
+lbl.pack(anchor='center')
+
+time()
+root.mainloop()
